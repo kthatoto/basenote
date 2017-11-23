5b073cecaec6ff7d2d5804799e870c3263d8831b
tk1to <tk02dr@gmail.com>
2017-11-24 05:02:45 +0900

    complete tutorial

--- app/src/app.js
+++ app/src/app.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  render() {
+    return (
+      <div>hello!</div>
+    );
+  }
+}
+// ========================================
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <App />,
   document.getElementById('app')
 );
