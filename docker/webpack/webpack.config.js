c36d958b7c29b1f754b1a8d6bd980a558189ad31
tk1to <tk02dr@gmail.com>
2017-11-24 17:46:08 +0900

    scss コンパイルテスト

--- docker/webpack/webpack.config.js
+++ docker/webpack/webpack.config.js
@@ -3,22 +3,27 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    bundle: './src/app.js'
+    bundle: './src/app.js',
+    css: './src/styles/main.scss',
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].js'
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
       {
+        test: /\.js[x]?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
-        test: /\.js[x]?$/,
         query: {
           cacheDirectory: true,
           presets: ['react', 'es2015']
         }
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
       }
     ]
   }
