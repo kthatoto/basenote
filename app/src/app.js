40cc3755a47d6b463ea78cb9e265b49c94653437
tk1to <tk02dr@gmail.com>
2017-11-27 15:23:34 +0900

    skeltonen

--- app/src/app.js
+++ app/src/app.js
@@ -1,49 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Menu } from './menu';
-import { Tasks } from './tasks';
-import { Projects } from './projects';
-import './styles/main.scss'
+
+import axiox from 'axios';
+
+import './styles/main.scss';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      content: 'tasks',
-    }
-  }
-  handleClick(i) {
-    let content;
-    if (i == 0) {
-      content = 'tasks';
-    } else {
-      content = 'projects';
-    }
-    this.setState({
-      content: content,
-    });
+      value: 0,
+    };
   }
   render() {
-    const content = this.state.content;
-    let contentComponent;
-    if (content === 'tasks') {
-      contentComponent = <Tasks/>;
-    } else {
-      contentComponent = <Projects/>;
-    }
     return (
-      <div>
-        <Menu
-          onClick={(i) => this.handleClick(i)}
-        />
-        <div id="content">
-          { contentComponent }
-        </div>
-      </div>
+      <div>value: {this.state.value}</div>
     );
   }
 }
-// ========================================
 
 ReactDOM.render(
   <App />,
