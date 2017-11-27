0c457db74db93ea8ec650303a65bf157862ed6e5
tk1to <tk02dr@gmail.com>
2017-11-27 14:15:18 +0900

    rails app

--- app/src/projects.js
+++ app/src/projects.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export class Projects extends React.Component {
+  renderSquare() {
+    return (
+      <li>projects</li>
+    );
+  }
   render() {
     return (
       <div>
         <ul>
-          <li>projects</li>
-          <li>projects</li>
-          <li>projects</li>
-          <li>projects</li>
-          <li>projects</li>
+          {this.renderSquare()}
+          {this.renderSquare()}
+          {this.renderSquare()}
+          {this.renderSquare()}
         </ul>
       </div>
     );
