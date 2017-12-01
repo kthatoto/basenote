2402281893ddd6de4f6638a4ea02de081dd8a3cd
tk1to <tk02dr@gmail.com>
2017-12-01 21:46:01 +0900

    esc

--- app/src/components/Console/Interface.js
+++ app/src/components/Console/Interface.js
@@ -4,9 +4,18 @@ import { connect } from 'react-redux';
 
 class Interface extends Component {
   componentDidMount() {
-
+    this.refs.line.focus();
+    document.addEventListener("keydown", (e) => {
+      if (e.keyCode == 27 || (e.ctrlKey && e.keyCode == 219)) {
+        this.refs.line.focus()
+      }
+    });
   }
-  command() {
+  command(e) {
+    if (e.keyCode == 13) {
+      let line = this.refs.line;
+      line.value = '';
+    }
   }
   render() {
     return (
@@ -14,7 +23,8 @@ class Interface extends Component {
         <div className="interface__prompt">$</div>
         <input type="text"
           className="interface__input"
-          onKeyDown={this.command}
+          ref="line"
+          onKeyDown={(e) => this.command(e)}
         />
       </div>
     );
