40ba080f2b49a37242966394743d2d58557bd035
tk1to <tk02dr@gmail.com>
2017-12-04 20:07:47 +0900

    switch main component by current path

--- app/src/components/MainDisplay.js
+++ app/src/components/MainDisplay.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import Dailies from './MainDisplay/Dailies';
+import Projects from './MainDisplay/Projects';
+import Tasks from './MainDisplay/Tasks';
+
 class MainDisplay extends Component {
   render() {
+    const components = [
+      'Dailies',
+      'Projects',
+      'Tasks',
+    ];
+    const currentComponent = this.props.Terminal.currentDirectory.split('/')[1];
+    const isShows = components.map(component => component == currentComponent );
     return (
       <div className="mainDisplay">
+        {isShows[0] && <Dailies />}
+        {isShows[1] && <Projects />}
+        {isShows[2] && <Tasks />}
       </div>
     );
   }
--- app/src/components/MainDisplay/Projects.js
+++ app/src/components/MainDisplay/Projects.js
@@ -5,6 +5,7 @@ class Projects extends Component {
   render() {
     return (
       <div className="projects">
+      projects!!!
       </div>
     );
   }
