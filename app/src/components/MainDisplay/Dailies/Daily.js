50128f90f65b8a70ce0595309cd37acdbf5f3c70
tk1to <tk02dr@gmail.com>
2017-12-05 14:58:50 +0900

    add inner border & daily date label

--- app/src/components/MainDisplay/Dailies/Daily.js
+++ app/src/components/MainDisplay/Dailies/Daily.js
@@ -2,9 +2,26 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Daily extends Component {
+  componentDidMount() {
+    this.addTabInvalidation();
+    document.activeElement.blur();
+  }
+  componentWillUnmount() {
+    this.removeTabInvalidation();
+  }
+  addTabInvalidation() {
+    document.onkeydown = e => {
+      if (e.keyCode == 9) { return false; }
+    };
+  }
+  removeTabInvalidation() {
+    document.onkeydown = null;
+  }
   render() {
     return (
-      <div className="daily">test</div>
+      <div className="daily">
+        <div className="daily__dateLabel"></div>
+      </div>
     );
   }
 }
