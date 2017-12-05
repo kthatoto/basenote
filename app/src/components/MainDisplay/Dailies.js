9cb7b9c6bb495551f29979aa7422eb3e28bf22f1
tk1to <tk02dr@gmail.com>
2017-12-05 12:55:46 +0900

    add dailyList component

--- app/src/components/MainDisplay/Dailies.js
+++ app/src/components/MainDisplay/Dailies.js
@@ -3,46 +3,14 @@ import { connect } from 'react-redux';
 
 import { fetchDailies, shiftFocus } from '../../actions/dailiesActionCreator';
 import { changeDirectory } from '../../actions/directoriesActionCreator';
-import DailyItem from './Dailies/DailyItem';
+import DailyList from './Dailies/DailyList';
 import Daily from './Dailies/Daily';
 
 class Dailies extends Component {
-  componentDidMount() {
-    this.props.dispatch(fetchDailies());
-    document.activeElement.blur();
-    this.addEventToShiftFocus();
-  }
-  componentWillUnmount() {
-    this.removeEventToShiftFocus();
-  }
-  addEventToShiftFocus() {
-    const listener = e => {
-      if (e.keyCode == 74) {
-        this.props.dispatch(shiftFocus(true));
-      }
-      if (e.keyCode == 75) {
-        this.props.dispatch(shiftFocus(false));
-      }
-      if (e.keyCode == 13) {
-        const focusedDaily = this.props.Dailies.dailies.find(daily => daily.focused);
-        this.props.dispatch(changeDirectory(focusedDaily.year + '-' + focusedDaily.month + '-' + focusedDaily.date));
-        this.removeEventToShiftFocus();
-      }
-    };
-    document.addEventListener('keydown', listener);
-    this.setState({ listener });
-  }
-  removeEventToShiftFocus() {
-    document.removeEventListener('keydown', this.state.listener);
-  }
   render() {
     if (this.props.Terminal.currentDirectory.split('/').length <= 2) {
       return (
-        <div className="dailies">
-          {this.props.Dailies.dailies.map(daily => {
-            return <DailyItem daily={daily} key={daily.id} />
-          })}
-        </div>
+        <DailyList />
       );
     } else {
       return (
---
+++ app/src/components/MainDisplay/Dailies/DailyList.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { fetchDailies, shiftFocus } from '../../../actions/dailiesActionCreator';
+import { changeDirectory } from '../../../actions/directoriesActionCreator';
+import DailyItem from './DailyItem';
+
+class DailyList extends Component {
+  componentDidMount() {
+    this.props.dispatch(fetchDailies());
+    document.activeElement.blur();
+    this.addEventToShiftFocus();
+  }
+  componentWillUnmount() {
+    this.removeEventToShiftFocus();
+  }
+  addEventToShiftFocus() {
+    const listener = e => {
+      if (e.keyCode == 74) {
+        this.props.dispatch(shiftFocus(true));
+      }
+      if (e.keyCode == 75) {
+        this.props.dispatch(shiftFocus(false));
+      }
+      if (e.keyCode == 13) {
+        const focusedDaily = this.props.Dailies.dailies.find(daily => daily.focused);
+        this.props.dispatch(changeDirectory(focusedDaily.year + '-' + focusedDaily.month + '-' + focusedDaily.date));
+        this.removeEventToShiftFocus();
+      }
+    };
+    document.addEventListener('keydown', listener);
+    this.setState({ listener });
+  }
+  removeEventToShiftFocus() {
+    document.removeEventListener('keydown', this.state.listener);
+  }
+  render() {
+    return (
+      <div className="dailyList">
+        {this.props.Dailies.dailies.map(daily => {
+          return <DailyItem daily={daily} key={daily.id} />
+        })}
+      </div>
+    );
+  }
+}
+function mapStateToProps(state) {
+  return state;
+}
+function mapDispatchToProps(dispatch) {
+  return { dispatch };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DailyList);
